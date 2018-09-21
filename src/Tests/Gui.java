0b36ec47e9b797f78393b27f7fc87ed0a709d396
Nolan Lavender <nolanlavender@me.com>
2018-09-21 16:27:06 -0400

    Other Frames added

--- src/Tests/Gui.java
+++ src/Tests/Gui.java
@@ -1,10 +1,19 @@
 package Tests;
 
+import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.TextField;
 import java.awt.Toolkit;
 import javax.swing.*;
+import java.awt.event.*;
+
 
 public class Gui extends JFrame{
+	JButton button1;
+	JButton button2;
+	JButton button3;
 	
 	public static void main(String[]args){
 		
@@ -27,21 +36,37 @@ public class Gui extends JFrame{
 		this.setTitle("HittersAdvantage");
 		
 		JPanel thePanel = new JPanel();
-		JLabel label1 = new JLabel("Main Menu");
 		
-		thePanel.add(label1);
+		thePanel.setSize(this.getWidth(),this.getHeight());
+		thePanel.setLayout(new GridBagLayout());
+		GridBagConstraints gbc = new GridBagConstraints();
+		gbc.gridwidth = GridBagConstraints.REMAINDER;
+		gbc.fill = GridBagConstraints.HORIZONTAL;
+		
+	
+		
+		
+		
+		
+		button1 = new JButton("New Game");
+		button2 = new JButton("Team");
+		button3 = new JButton("Reports");
+		
+
+		
+		ListenForButton lForButton = new ListenForButton();
+		button1.addActionListener(lForButton);
+		button2.addActionListener(lForButton);
+		button3.addActionListener(lForButton);
+		
+		
+		
+		
+		thePanel.add(button1, gbc);
+		thePanel.add(button2,gbc);
+		thePanel.add(button3, gbc);
 		this.add(thePanel);
 		
-		JButton button1 = new JButton("New Game");
-		JButton button2 = new JButton("Team");
-		JButton button3 = new JButton("Reports");
-		
-		
-		
-		
-		thePanel.add(button1);
-		thePanel.add(button2);
-		thePanel.add(button3);
 		
 		
 		
@@ -51,5 +76,61 @@ public class Gui extends JFrame{
 		
 		this.setVisible(true);
 	}
-
+	
+	// implement listeners
+	private class ListenForButton implements ActionListener{
+		public void actionPerformed(ActionEvent e){
+		if (e.getSource() == button1){
+			JFrame newGame = new JFrame();
+			newGame.setSize(400,400);
+			Toolkit tk = Toolkit.getDefaultToolkit();
+			
+			Dimension dim = tk.getScreenSize();
+			int xPos = (dim.width/2 ) - (newGame.getWidth()/2);
+			int yPos = (dim.height/2 ) - (newGame.getHeight()/2);
+			newGame.setLocation(xPos,yPos);
+			newGame.setResizable(false);
+			
+			newGame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			newGame.setTitle("Game");
+			
+			newGame.setVisible(true);
+			
+		}
+		if (e.getSource() == button2){
+			JFrame team = new JFrame();
+			team.setSize(400,400);
+			Toolkit tk = Toolkit.getDefaultToolkit();
+			
+			Dimension dim = tk.getScreenSize();
+			int xPos = (dim.width/2 ) - (team.getWidth()/2);
+			int yPos = (dim.height/2 ) - (team.getHeight()/2);
+			team.setLocation(xPos,yPos);
+			team.setResizable(false);
+			
+			team.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			team.setTitle("Team");
+		
+			team.setVisible(true);
+			
+		}
+		if (e.getSource() == button3){
+			JFrame report = new JFrame();
+			report.setSize(400,400);
+			Toolkit tk = Toolkit.getDefaultToolkit();
+			
+			Dimension dim = tk.getScreenSize();
+			int xPos = (dim.width/2 ) - (report.getWidth()/2);
+			int yPos = (dim.height/2 ) - (report.getHeight()/2);
+			report.setLocation(xPos,yPos);
+			report.setResizable(false);
+			
+			report.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			report.setTitle("Report");
+			
+			report.setVisible(true);
+			
+		}
+	}
+	}
 }
